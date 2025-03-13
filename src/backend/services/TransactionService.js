a03c36f6185ede12d15b0f4d045f9ca425942ab6
Arad Fadaei <arad@fadaei.dev>
2025-03-13 19:45:06 -0400

    Rename

--- src/backend/services/TransactionService.js
+++ src/backend/services/TransactionService.js
@@ -1,4 +1,4 @@
-const Transaction = require('../models/Transaction');
+const Transaction = require('../models/TransactionModel');
 const { sequelize } = require('../database'); 
 
 class TransactionService {
